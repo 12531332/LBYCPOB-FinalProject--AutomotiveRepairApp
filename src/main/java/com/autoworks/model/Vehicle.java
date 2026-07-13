25d989e1fb8ec5645a7231bb63a9bf111545c0b8
12508799 <angelo_jagonap@dlsu.edu.ph>
2026-07-13 15:04:48 +0800

    (ANGELO) --- Created abstract class "Vehicle"

--- src/main/java/com/autoworks/model/Vehicle.java
+++ src/main/java/com/autoworks/model/Vehicle.java
@@ -1,4 +1,20 @@
 package com.autoworks.model;
 
-public class Vehicle {
+public abstract class Vehicle {
+
+    private int wheelCount;
+    private String vehicleClass;
+
+    public Vehicle(String vehicleClass, int wheelCount) {
+        this.vehicleClass = vehicleClass;
+        this.wheelCount = wheelCount;
+    }
+
+    public String getVehicleClass() { return vehicleClass; }
+
+    public void setVehicleClass(String vehicleClass) { this.vehicleClass = vehicleClass; }
+
+    public int getWheelCount() { return wheelCount; }
+
+    public void setWheelCount(int wheelCount) { this.wheelCount = wheelCount; }
 }
